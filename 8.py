aad82cfd6d695e8b3e729a072a2f09e027a73431
Anya Poliakova <32990006+anyapoliakova@users.noreply.github.com>
2020-12-13 17:46:38 +0300

    Add files via upload
    :)

--- 8.py
+++ 8.py
@@ -1,28 +1,21 @@
+def char_range(c1, c2):
+    for c in range(ord(c1), ord(c2)+1):
+        yield chr(c)
+
 def super_reduced_string(text):
-    found = True
     res = text
+    found = True
     while found:
-        (res, found) = remove(res)
-    return res if len(res) > 0 else 'Empty String'
+        found = False
+        for c in char_range('a', 'z'):
+            if c * 2 in res:
+                found = True
+                res = res.replace(c * 2, '')
+    return res if res != '' else 'Empty String'
 
-def remove(text):
-    res = ''
-    found = False
-    i = 0
-    while i < len(text)-1:
-        if text[i] == text[i+1]:
-            i += 2
-            found = True
-        else:
-            res += text[i]
-            i += 1
-    if len(text) >= 2 and text[len(text) -1] != text[len(text) - 2]:
-        res += text[len(text) - 1]
-    return (res, found)
+print(super_reduced_string("cccxllyyy"))
+print(super_reduced_string("aa"))
+print(super_reduced_string("baab"))
+print(super_reduced_string("fghiiijkllmnnno"))
+print(super_reduced_string("chklssstt"))
 
-
-# print(super_reduced_string('aaabb'))
-line = 'abbcd'
-pairs = [(line[i], line[i+1]) for i in range(0, len(line) - 1)]
-filtered = list(filter(lambda p: p[0] != p[1], pairs))
-print(''.join([p[0] for p in filtered]) + filtered[-1][1])
